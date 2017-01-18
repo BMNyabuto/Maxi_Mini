bf065ac57f752f5c505da82d2609958d0fa24948
boman <bmnyabuto@gmail.com>
2017-01-19 00:54:24 +0300

    Added an operation that inserts the min element into the empty list

--- Maxi_min.py
+++ Maxi_min.py
@@ -3,3 +3,6 @@ def find_max_min(ArrayValues):
  	if isinstance(ArrayValues, list) == True:
  		ArrayValues.sort()
  		minValue = ArrayValues[0]
+ 		maxValue = ArrayValues[-1]
+
+ 	Result.append(minValue)
